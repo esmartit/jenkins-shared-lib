afd4630160952f942a5893f71b0f0c870c27e3a2
Gustavo Rodriguez <gustavo.rodriguez@adevinta.com>
2020-03-23 00:01:43 +0100

    fix: server port

--- vars/gradleDeliveryPipeline.groovy
+++ vars/gradleDeliveryPipeline.groovy
@@ -72,7 +72,7 @@ def call(body) {
                                 if (exists) {
                                     def version = readFile('version.txt').toString().replaceAll("[\\n\\t ]", "")
                                     sh "rm version.txt"
-                                    git branch: 'gh-pages', credentialsId: 'github', url: repoUrl
+                                    sh "git checkout --track origin/gh-pages"
                                     sh "git config --global user.email 'tech@esmartit.es'"
                                     sh "git config --global user.name 'esmartit'"
                                     def versionedArtifactName = "$artifactName-${version}.tgz"
